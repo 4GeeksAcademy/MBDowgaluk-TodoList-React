e648077ebb3666a142ec33533b7e7ff999bb0cfa
Merlina Barbara Dowgaluk <merlina.dowgaluk@gmail.com>
2023-11-06 20:11:16 +0000

    add form

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,32 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [ inputValue, setInputValue ] = useState("");
+	const [ toDos, setToDos ] = useState([]);
+
+	const handleInputValue = (e) => {
+		setInputValue(e.target.value)
+	}
+
+	const addInputValue = (e) => {
+		if (e.key === "Enter"){
+			setToDos(toDos.concat(inputValue));
+			setInputValue("")
+		}
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<h1>My ToDo</h1>
+			<ul>
+				<li><input type="text" placeholder="What do you need to do?" value={inputValue} onChange={handleInputValue} onKeyDown={addInputValue}></input></li>
+				{toDos.map((t, index) =>(
+					<li>{t.toUpperCase()}<i className="far fa-trash-alt m-1" onClick={() => setToDos(toDos.filter((t, currentIndex) => index != currentIndex))}></i></li>
+				))}
+			</ul>
+			<div className="taskTotal">{toDos.length} tasks left</div>
 		</div>
 	);
 };
